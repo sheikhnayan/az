8653d4b51e6ff46bab0ba76866238defa0eead8e
biddabari-dev <biddabarideveloper@gmail.com>
2024-06-15 22:40:16 +0600

    test

--- Modules/Customer/Routes/web.php
+++ Modules/Customer/Routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('customer')->group(function() {
     Route::get('/active-customer-list', 'CustomerController@customer_index')->name('cusotmer.list_active')->middleware(['auth','admin','permission']);
-    Route::get('/affiliate-customer-list', 'CustomerController@customer_affiliate')->name('cusotmer.affiliate_list')->middleware(['auth','admin']);
+    Route::get('/affiliate-customer-list', 'CustomerController@customer_affiliate')->name('cusotmer.list_affiliate')->middleware(['auth','admin']);
     Route::get('/customer-list/get-data', 'CustomerController@customer_index_get_data')->name('cusotmer.list.get-data')->middleware(['auth','admin']);
     Route::post('/is-active/update','CustomerController@update_active_status')->name('customer.update_active_status')->middleware(['auth','admin','permission','prohibited_demo_mode']);
 
